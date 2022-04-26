6eaf26feb58d697cc68ce9b5495a6618d2b34b70
salah <s7baijan@hotmail.com>
2022-04-26 22:56:13 +0300

    install wouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from "react"
 import logo from "./logo.svg"
 import { ChakraProvider } from "@chakra-ui/react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Link, Route } from "wouter"
 import { Home } from "./components/Home"
 function App() {
   return (
     <ChakraProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <Route path="/" component={Home} />
     </ChakraProvider>
   )
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,17 @@
+import { Box, Center, Text } from "@chakra-ui/react"
 import React from "react"
+import { Link } from "wouter"
 
 interface HomeProps {}
 
 export const Home: React.FC<HomeProps> = () => {
-  return <div>Home</div>
+  return (
+    <Center>
+      <Box>
+        <Link to={"/admin-login"}>
+          <Text fontSize={"3xl"}>Admin Login</Text>
+        </Link>
+      </Box>
+    </Center>
+  )
 }
