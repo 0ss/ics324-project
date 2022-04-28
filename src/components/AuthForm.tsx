7c2ca76e253055e7be55b811113df1db60a320b2
salah <s7baijan@hotmail.com>
2022-04-28 17:52:24 +0300

    recieve form value when submitted

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -7,7 +7,7 @@ import {
   Heading,
   Input,
 } from "@chakra-ui/react"
-import React from "react"
+import React, { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 interface AuthFormProps {
@@ -19,10 +19,18 @@ export const AuthForm: React.FC<AuthFormProps> = ({ form }) => {
     .map((e) => e.toLocaleLowerCase())
     .join(" ")
   const navigate = useNavigate()
+
+  const [email, setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log("email: ", email, ", password: ", password)
+    //supabase auth
+  }
   return (
     <Center backgroundColor={"gray.100"} h="100vh">
       <Box backgroundColor={"white"} borderRadius={"sm"} p={"12"}>
-        <form>
+        <form onSubmit={handleSubmit}>
           <Center py={"10"}>
             <Heading fontSize={"2xl"}>{heading}</Heading>
           </Center>
@@ -33,11 +41,17 @@ export const AuthForm: React.FC<AuthFormProps> = ({ form }) => {
               id="email"
               type={"email"}
               placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </FormControl>
           <FormControl isRequired>
-            <FormLabel htmlFor="password"> Password </FormLabel>
-            <Input borderColor={"black"} id="password" type={"password"} />
+            <FormLabel htmlFor="password">Password </FormLabel>
+            <Input
+              borderColor={"black"}
+              id="password"
+              type={"password"}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </FormControl>
           <Center py="5">
             <Button type={"submit"}>Submit</Button>
