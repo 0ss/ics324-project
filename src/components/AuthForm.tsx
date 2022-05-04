3e64db8a05807bebb748fad1e8f819e3ad341d8d
F4isalSH <f4isalcode@gmail.com>
2022-05-04 14:39:31 +0300

    Handling supabase client

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -6,27 +6,58 @@ import {
   FormLabel,
   Heading,
   Input,
-} from "@chakra-ui/react"
-import React, { useState } from "react"
-import { useNavigate } from "react-router-dom"
+  useToast,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { supabase } from "../supabaseClient";
 
 interface AuthFormProps {
-  form: "admin-login" | "admin-register" | "user-login" | "user-register"
+  form: "admin-login" | "admin-register" | "user-login" | "user-register";
 }
 export const AuthForm: React.FC<AuthFormProps> = ({ form }) => {
   const heading = form
     .split("-")
     .map((e) => e.toLocaleLowerCase())
-    .join(" ")
-  const navigate = useNavigate()
+    .join(" ");
+  const navigate = useNavigate();
 
-  const [email, setEmail] = useState<string>("")
-  const [password, setPassword] = useState<string>("")
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    console.log("email: ", email, ", password: ", password)
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [loading, setIsLoading] = useState<boolean>();
+  const toast = useToast();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("email: ", email, ", password: ", password);
     //supabase auth
-  }
+    try {
+      setIsLoading(true);
+      const { error } = await supabase.auth.signIn({ email, password });
+      if (error) throw error;
+      toast({
+        title: "Sign In Successfull",
+        position: "top",
+        description: "Check your email for the magic link :)",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+    } catch (err) {
+      toast({
+        title: "Error",
+        position: "top",
+        description: "Error has occurred, please try again.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+      setIsLoading(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Center backgroundColor={"gray.100"} h="100vh">
       <Box backgroundColor={"white"} borderRadius={"sm"} p={"12"}>
@@ -59,7 +90,7 @@ export const AuthForm: React.FC<AuthFormProps> = ({ form }) => {
           <Center py="5">
             <Button
               onClick={() => {
-                navigate("/")
+                navigate("/");
               }}
             >
               Home
@@ -68,5 +99,5 @@ export const AuthForm: React.FC<AuthFormProps> = ({ form }) => {
         </form>
       </Box>
     </Center>
-  )
-}
+  );
+};
