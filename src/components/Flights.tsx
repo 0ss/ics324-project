4ee5fd4e0ca02a2f4256f7c10d4b68abf481b080
salah <s7baijan@hotmail.com>
2022-05-04 22:22:13 +0300

    finish search bar

--- src/components/Flights.tsx
+++ src/components/Flights.tsx
@@ -1,14 +1,11 @@
 import {
   Box,
-  Button,
   Center,
-  Flex,
   FormControl,
   FormLabel,
   Heading,
   Input,
   SimpleGrid,
-  VStack,
 } from "@chakra-ui/react"
 import React, { useState } from "react"
 import { randomFlights } from "./constants"
@@ -18,14 +15,18 @@ interface FlightProps {}
 
 export const Flights: React.FC<FlightProps> = ({}) => {
   const [search, setSearch] = useState<string>("")
+  console.log(search)
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
   return (
-    <Box backgroundColor={"gray.100"} h="100%" w={"100%"}>
+    <Box backgroundColor={"gray.100"} h="100vh" w={"100%"}>
       <Heading py={"10"} textAlign={"center"}>
         Search Flights 🕊
       </Heading>
       <Box>
         <Box w={"40%"} mx={"auto"} alignItems="center">
-          <form>
+          <form onSubmit={handleSearch}>
             <FormControl isRequired>
               <FormLabel htmlFor="email"> email</FormLabel>
               <Input
@@ -40,19 +41,44 @@ export const Flights: React.FC<FlightProps> = ({}) => {
         </Box>
         <Center>
           <SimpleGrid columns={{ sm: 1, lg: 3 }}>
-            {randomFlights.map((e) => (
-              <Box m={"10"}>
-                <FlightCard
-                  aircraft_id={e.aircraft_id}
-                  arrival_time={e.arrival_time}
-                  depr_time={e.depr_time}
-                  from_location={e.from_location}
-                  id={e.id}
-                  to_location={e.to_location}
-                  price={e.price}
-                />
-              </Box>
-            ))}
+            {randomFlights.map((e) => {
+              if (search?.length > 0) {
+                if (
+                  (e.from_location as string)
+                    .toLocaleLowerCase()
+                    .includes(search) ||
+                  (e.to_location as string).toLocaleLowerCase().includes(search)
+                ) {
+                  return (
+                    <Box m={"10"} key={e.id}>
+                      <FlightCard
+                        aircraft_id={e.aircraft_id}
+                        arrival_time={e.arrival_time}
+                        depr_time={e.depr_time}
+                        from_location={e.from_location}
+                        id={e.id}
+                        to_location={e.to_location}
+                        price={e.price}
+                      />{" "}
+                    </Box>
+                  )
+                }
+                return null
+              }
+              return (
+                <Box m={"10"}>
+                  <FlightCard
+                    aircraft_id={e.aircraft_id}
+                    arrival_time={e.arrival_time}
+                    depr_time={e.depr_time}
+                    from_location={e.from_location}
+                    id={e.id}
+                    to_location={e.to_location}
+                    price={e.price}
+                  />{" "}
+                </Box>
+              )
+            })}
           </SimpleGrid>
         </Center>
       </Box>
